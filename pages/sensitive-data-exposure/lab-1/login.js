9ccac47358f348c4c454fc01f7309271fcdfb81b
ALdi Subarja <aldisubarja@gmail.com>
2025-07-16 15:00:23 +0700

    sensitive data exposure

--- pages/sensitive-data-exposure/lab-1/login.js
+++ pages/sensitive-data-exposure/lab-1/login.js
@@ -1,11 +1,15 @@
 
-
-fetch('login.php')
-    .then(response => response.json())
-    .then(data => {
-      localStorage.setItem('token', data.token);
-      alert('Login successful!');
-    })
-    .catch(error => {
-      alert('Login failed!');
-    });
+// Login handler (simplified client-side version)
+async function loginUser(email, password) {
+  fetch('login.php',{
+    body: JSON.stringify({ email, password }) 
+  })
+  .then(response => response.json())
+  .then(data => {
+    localStorage.setItem('token', data.token);
+    alert('Login successful!');
+  })
+  .catch(error => {
+    alert('Login failed!');
+  });
+}
